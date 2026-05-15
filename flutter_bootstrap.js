d6b980d79c993e94e6ffe408cd0d6c1abed58705
skypostal25-hash <skypostal25@gmail.com>
2026-05-15 18:10:57 +0100

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3003841753" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2821942915" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
